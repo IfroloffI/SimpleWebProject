c25f28d028c754cf29c99ed7e5c6a2e063e9ef7e
Zephrys_TheGreatest <froloff1830@gmail.com>
2023-10-23 15:11:31 +0300

    append React
    append React in project(FrontEnd)

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+function App() {
+  //axios.get();
+  useEffect(() => {
+    
+  }, []);
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
