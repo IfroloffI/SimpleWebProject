fda44c0375140ca5876b45ae2cd7a3565c59961a
Zephrys_TheGreatest <froloff1830@gmail.com>
2023-10-25 01:52:07 +0300

    Append output data, style
    Append some connect React with node projects, added output data from json file from server, updated css style of page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import axios from 'axios';
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect, useState } from 'react';
 
 function App() {
-  //axios.get();
+  const [listOfPosts, setListOfPosts] = useState([]);  
   useEffect(() => {
-    
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setListOfPosts(response.data);
+    });
   }, []);
   return (
-    <div className="App"></div>
+    <div className="App"> 
+    {listOfPosts.map((value, key) => {
+      return (
+        <div className="post"> 
+          <div className="title"> 
+            {value.title} 
+          </div>
+          <div className="body">
+            {value.postText}
+          </div>
+          <div className="footer">
+            {value.userName}
+          </div>
+        </div>
+      );
+    })}
+    </div>
   );
 }
 
