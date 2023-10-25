eee1989291c19300839367a5c4c32d448e34124e
Zephrys_TheGreatest <froloff1830@gmail.com>
2023-10-25 23:48:51 +0300

    append Yup
    add function getting info from client to server

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,28 +1,50 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function CreatePost() {
+  const initialValues = {
+    title: "",
+    postText: "",
+    userName: "",
+  };
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("You must input a Title!"),
+    postText: Yup.string().required(),
+    userName: Yup.string().min(3).max(15).required(),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="createPostPage">
-      <Formik>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
         <Form className="formContainer">
           <label>Title: </label>
+          <ErrorMessage name="title" component="span" />
           <Field
-            autocomplete="off"
+            autoComplete="off"
             id="inputCreatePost"
             name="title"
             placeholder="(Ex, Title...)"
           />
           <label>Post: </label>
+          <ErrorMessage name="posttext" component="span" />
           <Field
-            autocomplete="off"
+            autoComplete="off"
             id="inputCreatePost"
-            name="title"
+            name="postText"
             placeholder="(Ex, Post...)"
           />
           <label>User`s Name: </label>
+          <ErrorMessage name="userName" component="span" />
           <Field
-            autocomplete="off"
+            autoComplete="off"
             id="inputCreatePost"
             name="userName"
             placeholder="(Ex, Bob123...)"
