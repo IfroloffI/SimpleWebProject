4d102d0ccad75d4f0ea67a0570c84a7b84c3a81d
Zephrys_TheGreatest <froloff1830@gmail.com>
2023-10-26 00:41:38 +0300

    update style + append Post
    update css files, add Post.js with error of not finding local file

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import Post from ".pages/Post";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Link to="/">Home page</Link>
+        <div className="navbar">
+          <Link to="/">Home Page</Link>
+          <Link to="/createpost">Create A Post</Link>
+        </div>
         <Routes>
           <Route exact path="/" Component={Home} />
           <Route exact path="/createpost" Component={CreatePost} />
+          <Route exact path="/post/:id" Component={Post} />
         </Routes>
       </BrowserRouter>
     </div>
